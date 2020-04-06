d35d5cf4ece65d35b64d59406c56cd55cbd9d0c3
LukeLangius <lukelangius@gmail.com>
2020-04-05 18:29:11 -0600

    Changed around the areas, and fixed spelling errors.

--- src/FindShapes.java
+++ src/FindShapes.java
@@ -13,17 +13,20 @@ public class FindShapes {
         Circle circle = new Circle(3);
         Rhombus rhombus = new Rhombus(5, 0.5);
         Trapezoid trap = new Trapezoid(5,6,3,9,4);
-        System.out.println("Rectable are is: " + rec1.getArea());
+        Hexagon hex = new Hexagon(6);
+        Parallelogram para = new Parallelogram(6,8);
+        System.out.println("Rectangle are is: " + rec1.getArea());
         System.out.println("Square area is: "+ square.getArea());
-        System.out.println(trap.getArea());
-        System.out.println("The sum can go in either direction!");
-        System.out.println("The sum of the rectangel and square areas is: " + rec1.addArea(square));
-        System.out.println("The sum of the square and rectangle areas is: " + square.addArea(rec1));
-        System.out.println("The Circlwe area is: " + circle.getArea());
-        System.out.println("The Rhombus area is: " + rhombus.getArea());
-        System.out.println("The circle and retangle areas add to: " + circle.addArea(rec1));
-        System.out.println("The Rhombus and Square areas add to: " + rhombus.addArea(square));
-        System.out.println("The Rhombus and Cifcle areas add to: " + rhombus.addArea(circle));
+        System.out.println("Circle area is: " + circle.getArea());
+        System.out.println("Rhombus area is: " + rhombus.getArea());
+        System.out.println("Trapezoid area is: " + trap.getArea());
+        System.out.println("Hexagon area is: " + hex.getArea());
+        System.out.println("Parallelogram area is: " + para.getArea());
+        System.out.println("The areas of Rectangle and Square added together is: " + rec1.addArea(square));
+        System.out.println("The areas of Circle and Rhombus added together is: " + circle.addArea(rhombus));
+        System.out.println("The areas of Trapezoid and Hexagon added together is: " + trap.addArea(hex));
+        System.out.println("The areas of Hexagon and Parallelogram added together is: " + hex.addArea(para));
+
     }
 
 }
