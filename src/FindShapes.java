c14b7fe0db4864a61378be92388a98c72051ca42
LukeLangius <lukelangius@gmail.com>
2020-04-05 18:13:40 -0600

    I am adding a trapazoid to the project

--- src/FindShapes.java
+++ src/FindShapes.java
@@ -1,7 +1,7 @@
 /* FindShapes.java
  * Wayne Cook
  * 25 March 2020
- * Create the main class to test the rectangle, swuare, circle and shombus classes.
+ * Create the main class to test the rectangle, square, circle and rhombus classes.
  * Force a commit
  */
 //package Shape-RCC;
@@ -12,17 +12,17 @@ public class FindShapes {
         Square square = new Square(7);
         Circle circle = new Circle(3);
         Rhombus rhombus = new Rhombus(5, 0.5);
-        System.out.println("Rectable are is: " + rec1.getArea());
-        System.out.println("Square area is: "+ square.getArea());
-        System.out.println("The sum can go in either direction!");
-        System.out.println("The sum of the rectangel and square areas is: " + rec1.addArea(square));
-        System.out.println("The sum of the square and rectangle areas is: " + square.addArea(rec1));
-        System.out.println("The Circlwe area is: " + circle.getArea());
+        Trapazoid trap = new Trapazoid(3,5,7,4,4);
+        Parallelogram par = new Parallelogram(5,4);
+        System.out.println("The Rectangle are is: " + rec1.getArea());
+        System.out.println("The Square area is: " + square.getArea());
+        System.out.println("The Circle area is: " + circle.getArea());
         System.out.println("The Rhombus area is: " + rhombus.getArea());
-        System.out.println("The circle and retangle areas add to: " + circle.addArea(rec1));
-        System.out.println("The Rhombus and Square areas add to: " + rhombus.addArea(square));
-        System.out.println("The Rhombus and Cifcle areas add to: " + rhombus.addArea(circle));
-    }
-
+        System.out.println("The Trapezoid area is: " + trap.getArea());
+        System.out.println("The Parallelogram area is: " + par.getArea());
+        System.out.println("The Rectangle and Square ares add to: : " + rec1.addArea(square));
+        System.out.println("The Circle and Rhombus areas add to: " + circle.addArea(rhombus));
+        System.out.println("The Trapezoid and Parallelogram areas add to: " + trap.addArea(par));
+}
 }
 
