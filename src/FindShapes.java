fe2717227c6e7f7247e509d34ca4f65835ef434b
Alessandro <Alessandro.Krapf@cecstudents.org>
2020-04-05 18:13:25 -0600

    I added a hexagon class.

--- src/FindShapes.java
+++ src/FindShapes.java
@@ -12,16 +12,18 @@ public class FindShapes {
         Square square = new Square(7);
         Circle circle = new Circle(3);
         Rhombus rhombus = new Rhombus(5, 0.5);
-        System.out.println("Rectable are is: " + rec1.getArea());
+        Hexagon hexagon = new Hexagon(6);
+        System.out.println("Rectangle area is: " + rec1.getArea());
         System.out.println("Square area is: "+ square.getArea());
         System.out.println("The sum can go in either direction!");
-        System.out.println("The sum of the rectangel and square areas is: " + rec1.addArea(square));
+        System.out.println("The sum of the rectangle and square areas is: " + rec1.addArea(square));
         System.out.println("The sum of the square and rectangle areas is: " + square.addArea(rec1));
-        System.out.println("The Circlwe area is: " + circle.getArea());
+        System.out.println("The Circle area is: " + circle.getArea());
         System.out.println("The Rhombus area is: " + rhombus.getArea());
-        System.out.println("The circle and retangle areas add to: " + circle.addArea(rec1));
+        System.out.println("The circle and rectangle areas add to: " + circle.addArea(rec1));
         System.out.println("The Rhombus and Square areas add to: " + rhombus.addArea(square));
-        System.out.println("The Rhombus and Cifcle areas add to: " + rhombus.addArea(circle));
+        System.out.println("The Rhombus and Circle areas add to: " + rhombus.addArea(circle));
+        System.out.println("The Hexagon area is: " + hexagon.getArea());
     }
 
 }
